e0d989424608acca12bb895f18b72b36c696a9d4
moonyeol <ansduf05@gmail.com>
2020-12-22 18:15:44 +0900

    save update find

---
+++ src/main/java/com/moon/toyproject/springboot/web/PostsApiController.java
@@ -0,0 +1,31 @@
+package com.moon.toyproject.springboot.web;
+
+import com.moon.toyproject.springboot.service.posts.PostsService;
+import com.moon.toyproject.springboot.web.dto.PostsResponseDto;
+import com.moon.toyproject.springboot.web.dto.PostsSaveRequestDto;
+import com.moon.toyproject.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.PostRemove;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
