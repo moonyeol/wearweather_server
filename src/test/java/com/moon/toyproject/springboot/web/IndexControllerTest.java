e66abfa2edbd0d11315b6d67e4d71ec9a117a4fd
moonyeol <ansduf05@gmail.com>
2021-01-29 22:19:57 +0900

    배포 테스트

--- src/test/java/com/moon/toyproject/springboot/web/IndexControllerTest.java
+++ src/test/java/com/moon/toyproject/springboot/web/IndexControllerTest.java
@@ -22,6 +22,6 @@ public class IndexControllerTest {
     public void 메인페이즈_로딩(){
         String body =this.restTemplate.getForObject("/",String.class);
 
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스 Ver.2");
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스 Ver.3");
     }
 }
