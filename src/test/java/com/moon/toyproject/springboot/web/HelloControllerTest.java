f8767a81c66c97ce465c2392be16bc16b8e89528
moonyeol <ansduf05@gmail.com>
2021-01-31 18:51:26 +0900

    테스트

--- src/test/java/com/moon/toyproject/springboot/web/HelloControllerTest.java
+++ src/test/java/com/moon/toyproject/springboot/web/HelloControllerTest.java
@@ -36,7 +36,7 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
     }
-/*
+
     @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception{
@@ -50,6 +50,6 @@ public class HelloControllerTest {
                 .andExpect(jsonPath("$.name",is(name)))
                 .andExpect(jsonPath("$.amount",is(amount)));
     }
-*/
+
 
 }
--- src/test/java/com/moon/toyproject/springboot/web/IndexControllerTest.java
+++ src/test/java/com/moon/toyproject/springboot/web/IndexControllerTest.java
@@ -17,11 +17,11 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 public class IndexControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
-
+/*
     @Test
     public void 메인페이즈_로딩(){
         String body =this.restTemplate.getForObject("/",String.class);
 
         assertThat(body).contains("스프링 부트로 시작하는 웹 서비스 Ver.2");
-    }
+    }*/
 }
